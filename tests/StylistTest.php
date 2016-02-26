a9b86f21dffca6d13a12ffdc9c88a27ad17db237
jwnardine <jwnardine@gmail.com>
2016-02-26 11:40:16 -0800

    add getAll save and deleteAll to Stylist.php and tests to StylistTest to make pass

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -27,5 +27,58 @@
             //Assert
             $this->assertEquals($name, $result);
         }
+
+        function test_getId()
+       {
+           //Arrange
+           $name = "Sandra Styles";
+           $id = 1;
+           $test_stylist = new Stylist($name, $id);
+           //Act
+           $result = $test_stylist->getId();
+           //Assert
+           $this->assertEquals(true, is_numeric($result));
+       }
+       function test_save()
+       {
+            //Arrange
+            $name = "Sandra Styles";
+            $test_stylist = new Stylist($name);
+            $test_stylist->save();
+            //Act
+            $result = Stylist::getAll();
+            //Assert
+            $this->assertEquals($test_stylist, $result[0]);
+        }
+        function test_getAll()
+       {
+           //Arrange
+           $name = "Sandra Styles";
+           $name2 = "Michael Styles";
+           $test_stylist = new Stylist($name);
+           $test_stylist->save();
+           $test_stylist2 = new Stylist($name2);
+           $test_stylist2->save();
+           //Act
+           $result = Stylist::getAll();
+           //Assert
+           $this->assertEquals([$test_stylist, $test_stylist2], $result);
+       }
+       function test_deleteAll()
+       {
+            //Arrange
+            $name = "Sandra Styles";
+            $name2 = "Michael Styles";
+            $test_stylist = new Stylist($name);
+            $test_stylist->save();
+            $test_stylist2 = new Stylist($name2);
+            $test_stylist2->save();
+            //Act
+            Stylist::deleteAll();
+            $result = Stylist::getAll();
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
     }
  ?>
